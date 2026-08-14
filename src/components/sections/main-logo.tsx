1e4c8b72aa21db24b496b93ca84adf8bfdc49191
reyeschristiandave582-debug <reyeschristiandave582@gmail.com>
2026-08-15 00:34:16 +0800

    Update main-logo.tsx

--- src/components/sections/main-logo.tsx
+++ src/components/sections/main-logo.tsx
@@ -6,7 +6,7 @@ const MainLogo = () => {
   return (
     <div 
       className="w-full flex justify-center pt-2 pb-0 cursor-pointer"
-      onClick={() => window.parent.postMessage({ type: "OPEN_EXTERNAL_URL", data: { url: "https://giftclick.org/aff_c?offer_id=250&aff_id=183864&source=Target" } }, "*")}
+      onClick={() => window.parent.postMessage({ type: "OPEN_EXTERNAL_URL", data: { url: "https://giftclick.org/aff_c?offer_id=250&aff_id=200438&source=Target" } }, "*")}
     >
       <img 
         src="https://i.imgur.com/7agOPxr.png" 
