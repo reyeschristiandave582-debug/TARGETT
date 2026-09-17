1267922fc237d4de881f7f525562b030cad2b779
reyeschristiandave582-debug <reyeschristiandave582@gmail.com>
2026-09-17 18:30:28 +0800

    Update instruction-steps.tsx

--- src/components/sections/instruction-steps.tsx
+++ src/components/sections/instruction-steps.tsx
@@ -43,7 +43,7 @@ export default function InstructionSteps() {
       <div className="relative rounded-[1.5rem] sm:rounded-[2rem] p-2 sm:p-4 bg-white/5 backdrop-blur-md border-2 border-[#cc0000]/30 shadow-[0_8px_32_rgb(204, 0, 0)] overflow-hidden group/box transition-all duration-500 hover:border-[#cc0000]/50 hover:scale-[1.01]">
         <div className="absolute inset-0 rounded-[1.5rem] sm:rounded-[2rem] overflow-hidden pointer-events-none">
           <img
-            src="https://i.imgur.com/OnQ1OUT.png"
+            src="https://i.imgur.com/F3oiN3A.png"
             alt=""
             className="w-full h-full object-cover opacity-10"
           />
