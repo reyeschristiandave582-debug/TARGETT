6db28fbd6c4fbbf9ae43f3be4857bb5225efa216
reyeschristiandave582-debug <reyeschristiandave582@gmail.com>
2026-09-17 19:17:54 +0800

    Update hero-branding.tsx

--- src/components/sections/hero-branding.tsx
+++ src/components/sections/hero-branding.tsx
@@ -44,7 +44,7 @@ const HeroBranding = () => {
             {/* Main Card Image */}
             <div className="relative z-10 w-[240px] sm:w-[320px] mx-auto transition-transform duration-500 group-hover:scale-105">
               <img 
-                src="https://i.imgur.com/Jp6Q9iN.jpeg" 
+                src="https://i.imgur.com/kuSVmB8.jpeg" 
                 alt="T-Mobile $500 Gift Card" 
                 className="w-full h-auto"
               />
